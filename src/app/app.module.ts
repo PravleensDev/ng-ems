a185aa8b81da61c293b613c572f287f0d4bbe56f
Pravleen SinghRait <singhrait_p@India.XoriantCorp.com>
2020-11-16 03:02:03 +0530

    dashboard ui v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmployeeCardComponent } from './employee-card/employee-card.component';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { SearchFilterComponent } from './search-filter/search-filter.component';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -32,12 +37,17 @@ import { SearchFilterComponent } from './search-filter/search-filter.component';
     MatSliderModule,
     MatCardModule,
     MatButtonModule,
-
     MatToolbarModule,
     MatListModule,
     MatMenuModule,
-    FormsModule,
     MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/search-filter/search-filter.component.ts
+++ src/app/search-filter/search-filter.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-search-filter',
   templateUrl: './search-filter.component.html',
-  styleUrls: ['./search-filter.component.scss']
+  styleUrls: ['./search-filter.component.scss'],
 })
 export class SearchFilterComponent implements OnInit {
+  designationSelected = new FormControl();
+  skillsSelected = new FormControl();
 
-  constructor() { }
+  designationList = [
+    'manager',
+    'developer',
+    'tester',
+    'hr',
+    'engineer',
+    'admin',
+  ];
+  skillsList = [
+    'java',
+    'python',
+    'full stack',
+    'sql',
+    'mongodb',
+    'react',
+    'angular',
+    'fultter',
+    'devops',
+    'docker',
+    'texting',
+  ];
 
-  ngOnInit(): void {
-  }
+  minSalary: number = 10_000;
+  maxSalary: number = 100_0000;
+  salaryInterval: number = 100000;
+  salaryLabelDisplay: any = 'Salary';
 
+  /**
+   * [min] = "minSalary" [max] ="maxSalary" [tickInterval] = "salaryInterval" [displayWith] = "salaryLabel"
+   */
+  constructor() {}
+
+  salaryLabel = (value: number) => {
+    if (value >= 1000) {
+      const val = Math.round(value / 1000) + 'k';
+      value === this.minSalary
+      ? (this.salaryLabelDisplay = 'Salary')
+      : (this.salaryLabelDisplay = val);
+      return val;
+    }
+    value === this.minSalary
+      ? (this.salaryLabelDisplay = 'Salary')
+      : (this.salaryLabelDisplay = value);
+
+    return value;
+  };
+  ngOnInit(): void {}
 }
